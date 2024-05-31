1f0fc6895462daf8d5c97942d477452f14426b30
Your Name <you@example.com>
2024-05-31 11:09:12 +0200

    sauvegarde

--- migrations/Version20240531085718.php
+++ migrations/Version20240531085718.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240530074655 extends AbstractMigration
+final class Version20240531085718 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,13 @@ final class Version20240530074655 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE competence (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE formation (id INT AUTO_INCREMENT NOT NULL, niveau_id INT NOT NULL, category_id INT NOT NULL, nom_etablissement VARCHAR(255) NOT NULL, ville_etablissement VARCHAR(255) NOT NULL, distanciel TINYINT(1) NOT NULL, date_d DATETIME NOT NULL, date_f DATETIME DEFAULT NULL, nom_formation VARCHAR(255) NOT NULL, specialite VARCHAR(255) NOT NULL, INDEX IDX_404021BFB3E9C81 (niveau_id), INDEX IDX_404021BF12469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE niveau (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE veille (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, nom VARCHAR(255) NOT NULL, date_d DATETIME NOT NULL, acquisition VARCHAR(255) NOT NULL, veille_continue TINYINT(1) NOT NULL, INDEX IDX_CE188CF312469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE formation ADD CONSTRAINT FK_404021BFB3E9C81 FOREIGN KEY (niveau_id) REFERENCES niveau (id)');
         $this->addSql('ALTER TABLE formation ADD CONSTRAINT FK_404021BF12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
         $this->addSql('ALTER TABLE veille ADD CONSTRAINT FK_CE188CF312469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
@@ -33,7 +38,12 @@ final class Version20240530074655 extends AbstractMigration
         $this->addSql('ALTER TABLE formation DROP FOREIGN KEY FK_404021BFB3E9C81');
         $this->addSql('ALTER TABLE formation DROP FOREIGN KEY FK_404021BF12469DE2');
         $this->addSql('ALTER TABLE veille DROP FOREIGN KEY FK_CE188CF312469DE2');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE competence');
         $this->addSql('DROP TABLE formation');
+        $this->addSql('DROP TABLE niveau');
+        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE veille');
+        $this->addSql('DROP TABLE messenger_messages');
     }
 }
